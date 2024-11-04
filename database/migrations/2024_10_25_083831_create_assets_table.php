56123d962ef4b467e71a1313e075d3b45af0de3d
HaiqalAzry <haiqalazry@puncakniaga.com.my>
2024-11-04 10:12:30 +0800

    feat: add tables

--- database/migrations/2024_10_25_083831_create_assets_table.php
+++ database/migrations/2024_10_25_083831_create_assets_table.php
@@ -18,8 +18,20 @@ return new class extends Migration
             $table->string('asset_name');
             $table->string('serial_number')->unique();
             $table->string('asset_no')->unique();
-            $table->string('location');
             $table->string('brand');
+            $table->string('model');
+            $table->string('type');
+            $table->string('spec');
+            $table->string('model');
+            $table->string('domain');
+            $table->string('location');
+            $table->string('company_id');
+            $table->string('department_id');
+            $table->string('user_id');
+            $table->string('previous_user_id');
+            $table->string('paid_by');
+            $table->string('condition');
+            $table->string('remark');
             $table->timestamps();
         });
     }
